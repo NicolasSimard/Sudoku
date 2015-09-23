0df14907597e8cdd2ca00e401432997e344c9b61
Nicolas Simard <simard.nicolas.0@gmail.com>
2015-09-23 13:39:45 -0400

    Corrected a little bug, but still not working!

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,10 +1,19 @@
+verb = False
+
 turn = 0
+pointer = (0,0)
 
 def sudoku_print():
+    print("----------------------")
     for i in range(9):
+        print("|", end="")
         for j in range(9):
-            print(sudoku[(i,j)]["value"][0],end="")
+            print(sudoku[(i,j)]["value"][0],
+                 ("*" if (i,j) == pointer else " "),
+                 ("|" if j%3 == 2 else ""),
+                 end="", sep="")
         print()
+        if i%3 == 2: print("----------------------")
 
 #Returns all the cells in the same row as (i,j) (excluding it)
 def row(i,j):
@@ -23,37 +32,49 @@ def block(i,j):
 def neighbors(i,j):
     return row(i,j) + col(i,j) + block(i,j)
 
-def set(cell, value):
-    global sudoku
-    cell["value"] = (value, turn)
-    for coord in cell["neighbors"]:
-        if value in sudoku[coord]["possibilities"].keys():
-            del sudoku[coord]["possibilities"][value]
-        if len(sudoku[coord]["possibilities"]) == 1:
-            v = list(sudoku[coord]["possibilities"].keys())[0]
-            del sudoku[coord]["possibilities"][v]
-            set(sudoku[coord], v)
+def set(coord, value):
+    global sudoku, pointer, verb
+    pointer = coord
+    if verb:
+        print("Setting cell",sudoku[coord]["coord"],"to",value)
+        print("Number of possibilities:",len(sudoku[coord]["possibilities"]))
+    sudoku[coord]["value"] = (value, turn)
+    sudoku[coord]["possibilities"] = {}
+    for ij in sudoku[coord]["neighbors"]:
+        pointer = ij
+        if verb:
+            sudoku_print()
+            input()
+        if value in sudoku[ij]["possibilities"].keys():
+            del sudoku[ij]["possibilities"][value]
+        if len(sudoku[ij]["possibilities"]) == 1:
+            v = list(sudoku[ij]["possibilities"].keys())[0]
+            set(ij, v)
 
 #1 Read the Sudoku puzzle
 sudoku = {}
-initial_cells = []
+initial_coord = []
 i = 0
 file = open("easy1.txt","r")
 for line in file:
     values = list(map(int,line.split()))
     for j in range(9):
         sudoku[(i,j)] = {
+            "coord":(i,j),
             "value":(values[j],turn),
             "possibilities":{1:None,2:None,3:None,4:None,5:None,6:None,7:None,8:None,9:None},
             "neighbors":neighbors(i,j)
         }
-        if not sudoku[(i,j)]["value"] == 0: initial_cells.append((i,j))
+        if sudoku[(i,j)]["value"][0] != 0: initial_coord.append((i,j))
     i += 1
 
 sudoku_print()
 
 #Initialize the sudoku with the initial values
-for coord in initial_cells:
-    set(sudoku[coord],sudoku[coord]["value"][0])
+for coord in initial_coord:
+    if verb: print("In init loop with cell",coord)
+    set(coord,sudoku[coord]["value"][0])
 
 sudoku_print()
+for coord, cell in sudoku.items():
+    print(len(cell["possibilities"]))
