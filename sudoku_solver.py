9215533a72455161ec4e166f4e37feec1eab0559
Nicolas Simard <simard.nicolas.0@gmail.com>
2015-09-25 08:47:50 -0400

    Simplified version of the first step of the solver.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -8,76 +8,45 @@ def sudoku_print():
     for i in range(9):
         print("|", end="")
         for j in range(9):
-            print(sudoku[(i,j)]["value"][0],
+            print(sudoku[(i,j)][0],
                  ("*" if (i,j) == pointer else " "),
                  ("|" if j%3 == 2 else ""),
                  end="", sep="")
         print()
         if i%3 == 2: print("----------------------")
 
-#Returns all the cells in the same row as (i,j) (excluding it)
-def row(i,j):
-    return [(i,l) for l in range(9) if not l == j]
-
-#Returns all the cells in the same column as (i,j) (excluding it)
-def col(i,j):
-    return [(k,j) for k in range(9) if not k == i]
-
-#Returns all then cells in the same block as (i,j) (excluding cells above)
-def block(i,j):
-    return [(k + 3*(i//3),l + 3*(j//3)) for k in range(3) for l in range(3)
-                                        if not (k == i%3 or l == j%3)]
-
-def neighbors(i,j):
-    return row(i,j) + col(i,j) + block(i,j)
+def neighbors(i,j): # neighbors include (i,j)
+    return [(k,l) for k in range(9) for l in range(9) if
+            (k == i or l == j or (k//3 == i//3 and l//3 == j//3))]
 
 def set(coord, value):
-    global sudoku, pointer, verb
-    if verb:
-        pointer = coord
-        print("Setting cell",sudoku[coord]["coord"],"to",value)
-        print("Number of possibilities:",len(sudoku[coord]["possibilities"]))
-    sudoku[coord]["value"] = (value, turn)
-    sudoku[coord]["possibilities"] = {}
-    for ij in sudoku[coord]["neighbors"]:
-        if verb:
-            pointer = ij
-            sudoku_print()
-            input()
-        if value in sudoku[ij]["possibilities"].keys():
-            del sudoku[ij]["possibilities"][value]
-        if len(sudoku[ij]["possibilities"]) == 1:
-            v = list(sudoku[ij]["possibilities"].keys())[0]
-            set(ij, v)
+    global sudoku, pointer
+    sudoku[coord] = [value,[]]
+    for ij in neighbors(*coord):
+        if value in sudoku[ij][1]: sudoku[ij][1].remove(value)
+        if len(sudoku[ij][1]) == 1: set(ij, *sudoku[ij][1])
 
+import time
+start = time.time()
 
-#1 Read the Sudoku puzzle
+#2 Initialise empty sudoku
 sudoku = {}
-initial_coord = []
+for coord in [(i,j) for i in range(9) for j in range(9)]:
+    sudoku[coord] = [0,list(range(1,10))]
+
 i = 0
-file = open("hard1.txt","r")
+file = open("easy1.txt","r")
 for line in file:
-    values = list(map(int,line.split()))
-    for j in range(9):
-        sudoku[(i,j)] = {
-            "coord":(i,j),
-            "value":(values[j],turn),
-            "possibilities":{1:None,2:None,3:None,4:None,5:None,6:None,7:None,8:None,9:None},
-            "neighbors":neighbors(i,j)
-        }
-        if sudoku[(i,j)]["value"][0] != 0: initial_coord.append((i,j))
+    for x in enumerate(list(map(int,line.split()))):
+        if x[1] != 0: set((i,x[0]),x[1])
     i += 1
 
-print("Initial puzzle:")
+print("Initialisation in :",time.time()-start,"seconds")
 sudoku_print()
 
-#Initialize the sudoku with the initial values
-for coord in initial_coord:
-    if verb: print("In init loop with cell",coord)
-    set(coord,sudoku[coord]["value"][0])
 
-print("After initialization:")
-sudoku_print()
+# for coord in [(i,j) for i in range(9) for j in range(9) if sudoku[(i,j)] == 0]:
+#     set(coord,sudoku[coord][1].pop(),[])
 
 # for coord, cell in sudoku.items(): print(len(cell["possibilities"]))
 
