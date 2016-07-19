45239ea6b5ebfd7c5a3eb0257569b73af04b59a8
Nicolas Simard <simard.nicolas.0@gmail.com>
2016-07-19 15:36:11 -0400

    Version with fixed length stack instead of variable length.

--- solver.py
+++ solver.py
@@ -23,14 +23,15 @@ def print_sudoku(sudoku):
            # [sudoku[27*(n//27)+9*i+3*((n//3)%3)+j] for i in range(3) for j in range(3)]
            
 sudoku = []
-file = open("hardest1.txt","r")
+file = open("medium1.txt","r")
 for line in file:
     sudoku += list(map(int,line.split()))
 print_sudoku(sudoku)  
 
 
 nlist = [neigh(n,range(81)) for n in range(81)]    
-guess = []
+stack = [0 for i in range(81)]
+ptr = 0
 n,i = 0,1
 while 1:
     while n < 81 and sudoku[n] != 0: n+=1
@@ -39,11 +40,13 @@ while 1:
     #L = neigh(n,sudoku)
     while i <= 9 and i in L: i+=1
     if i <= 9:
-        guess += [(n,i)]
+        stack[ptr] = (n,i)
+        ptr += 1
         sudoku[n] = i
         i = 1
     else:
-        n,i = guess.pop()
+        ptr -= 1
+        n,i = stack[ptr]
         sudoku[n] = 0
         i += 1
     
