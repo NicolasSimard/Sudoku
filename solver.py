c72d355f70073e2a1e087749ad2a7a18cd06ee00
Nicolas Simard <simard.nicolas.0@gmail.com>
2016-07-19 21:02:11 -0400

    Change script to take sudoku as argument in command line.

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 # To test: python -m cProfile solver.py
+import sys
 
 def print_neigh(n):
     for row in range(9):
@@ -6,28 +7,27 @@ def print_neigh(n):
             print("*" if col in nlist[n] else "-",end = " ")
         print()
     print()
-  
+     
+# def neigh(n,sudoku):
+    # return [sudoku[(n+9*i)%81] for i in range(9)]+\
+           # [sudoku[9*(n//9)+i] for i in range(9)]+\
+           # [sudoku[27*(n//27)+9*i+3*((n//3)%3)+j] for i in range(3) for j in range(3)]
+
 def neigh(n,sudoku):
     return [sudoku[i] for i in range(81) if (n%9 == i%9 or n//9 == i//9 or (n//27 == i//27 and (n//3)%3 == (i//3)%3)) and n != i]
-    
+
 def print_sudoku(sudoku):
     for row in range(9):
         for col in range(9*row,9*row+9):
             print(sudoku[col],end = " ")
         print()
     print()
-
-# def neigh(n,sudoku):
-    # return [sudoku[(n+9*i)%81] for i in range(9)]+\
-           # [sudoku[9*(n//9)+i] for i in range(9)]+\
-           # [sudoku[27*(n//27)+9*i+3*((n//3)%3)+j] for i in range(3) for j in range(3)]
-           
+ 
 sudoku = []
-file = open("medium1.txt","r")
+file = open(sys.argv[1],"r")
 for line in file:
     sudoku += list(map(int,line.split()))
 print_sudoku(sudoku)  
-
 
 nlist = [neigh(n,range(81)) for n in range(81)]    
 stack = [0 for i in range(81)]
@@ -51,4 +51,3 @@ while 1:
         i += 1
     
 print_sudoku(sudoku)
-
